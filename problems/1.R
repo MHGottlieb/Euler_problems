bdb70d2a88e4d7a6809049aa6a2caf6f35675676
MHGottlieb <magnus.gottlieb@gmail.com>
2017-02-12 16:11:47 +0100

    Update 1.R

--- problems/1.R
+++ problems/1.R
@@ -1,6 +1,7 @@
 # Project Euler problem #1
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
+# See https://projecteuler.net/problem=1
 
   three <- seq(from=0, to=999, by=3)
   five <- seq(from=0, to=999, by=5)
